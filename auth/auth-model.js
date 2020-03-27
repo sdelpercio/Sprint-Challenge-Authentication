80db0fd15db215467d86b17b7689552d39a23afd
Shawn DelPercio <sdelperc@gmail.com>
2020-03-27 11:18:17 -0400

    added auth helpers

---
+++ auth/auth-model.js
@@ -0,0 +1,20 @@
+const db = require('../database/dbConfig');
+
+module.exports = {
+	insert,
+	findById
+};
+
+function insert(userInfo) {
+	return db('users')
+		.insert(userInfo)
+		.then(id => {
+			return findById(id);
+		});
+}
+
+function findById(id) {
+	return db('users')
+		.where({ id })
+		.first();
+}
